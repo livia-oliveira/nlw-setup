b5cd865fd6df0d7c5f07f80d83d6123419a5c708
Lívia Oliveira <91535618+livia-oliveira@users.noreply.github.com>
2023-01-28 09:20:40 -0300

    Aula 2 - Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { StatusBar} from 'react-native';
 import {
   useFonts,
   Inter_400Regular, 
@@ -9,6 +9,7 @@ import {
 
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,22 +26,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Moranguinho</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: 'hotpink',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
